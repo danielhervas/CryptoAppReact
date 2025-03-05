9d625751db8481a5ea77f2c9994e5b23add6ba2d
angelmrt <amartinr03@gmail.com>
2025-03-05 13:05:04 +0100

    cipote

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,17 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    
+    
+  /*  
+  Tocar rutas para luego hacerlo bien
+  index("routes/home.tsx")
+    layout("routes/anidados/Info.tsx", [
+        index("routes/anidados/home.tsx"),
+        route("barra", "routes/anidados/Dashboard.tsx", [
+          route("nieta", "routes/anidados/Nieta.tsx"),
+        ]),
+        route("cuartos", "routes/anidados/Finanzas.tsx"),
+      ]),
+    */
+] satisfies RouteConfig;
