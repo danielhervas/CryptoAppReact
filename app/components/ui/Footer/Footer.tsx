f28202aa45e5660453c8e2cb507cdf9fec3a9742
angelmrt <amartinr03@gmail.com>
2025-03-08 14:48:20 +0100

    footer apañado nacho cabron

--- app/components/ui/Footer/Footer.tsx
+++ app/components/ui/Footer/Footer.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import Logo from "../../../../public/v2logo.png";
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import InfoItem from './InfoItem';
 
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white pt-4 pb-4">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
         {/* Redes sociales */}
-       
+
 
         <div className="text-white text-xl font-bold flex-shrink-0 w-36 px-4">
-        <img src={Logo} alt="LogoCryptobrosV2" />
-      </div>
+          <img src={Logo} alt="LogoCryptobrosV2" />
+        </div>
 
         {/* Políticas */}
-        <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
-          <a href="#" className="text-white hover:text-gray-400">Política de Privacidad</a>
-          <a href="#" className="text-white hover:text-gray-400">Política de Cookies</a>
-          <a href="#" className="text-white hover:text-gray-400">Términos y Condiciones</a>
+        <nav className="flex space-x-4 m-0 p-0">
+          <ul className="flex space-x-6 text-xl pe-6">
+            <InfoItem name="Política de privacidad" href="/" />
+            <InfoItem name="Política de Cookies" href="/noticias" />
+            <InfoItem name="Términos y condiciones" href="/about" />
+          </ul>
+        </nav>
+
+        {/* 
+          <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
+          <InfoItem name="Política de Privacidad" href="#" />
+          <InfoItem name="Política de Cookies" href="#" />
+          <InfoItem name="Términos y Condiciones" href="#" />
         </div>
+         */}
 
         <div className="flex space-x-4 mb-4 sm:mb-0 pe-5 ps-5">
           <a href="#" aria-label="Facebook" className="text-white hover:text-gray-400">
--- app/routes/noticias.tsx
+++ app/routes/noticias.tsx
@@ -1,16 +1,17 @@
 import Header from "app/components/ui/Header/Header";
-
+import Footer from "app/components/ui/Footer/Footer";
 const Noticias = () => {
-  
-    return(
-  <>
-  <Header />
-  <div className="container mx-auto p-4">
-    <h1 className="text-2xl font-bold">News</h1>
-    <p className="text-lg">This is the News page</p>
-  </div>
-</>
-)
-  };
-  
-  export default Noticias;
+
+  return (
+    <>
+      <Header />
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold">News</h1>
+        <p className="text-lg">This is the News page</p>
+      </div>
+      <Footer />
+    </>
+  )
+};
+
+export default Noticias;
